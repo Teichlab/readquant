f1bdea0d772e71eb2e68e70834f4bcf75710bff5
Valentine Svensson <vale@ebi.ac.uk>
2017-03-17 18:14:58 +0000

    Changed defaults in scripts

--- scripts/gather_bio_qc.py
+++ scripts/gather_bio_qc.py
@@ -61,9 +61,9 @@ def get_accuracy(ercc, quant, det_threshold=0.1):
 
 
 @click.command()
-@click.argument('pattern')
-@click.argument('output')
-@click.option('--version')
+@click.argument('pattern', default='salmon/*_salmon_out')
+@click.argument('output', default='sample_bio_qc.csv')
+@click.option('--version', default='0.7.2')
 def main(pattern='salmon/*_salmon_out', output='sample_bio_qc.csv', version=None):
     ercc = np.log(get_ERCC())
     MT = get_MT()
@@ -78,10 +78,15 @@ def main(pattern='salmon/*_salmon_out', output='sample_bio_qc.csv', version=None
         qc_data = pd.Series()
         qc_data['detection_limit'] = get_detection_limit(ercc, quant)
         qc_data['accuracy'] = get_accuracy(ercc, quant)
-        qc_data['ERCC_content'] = quant[ercc.index].sum()
 
-        quant = quant.drop(ercc.index)
-        quant = quant / quant.sum() * 1e6
+        try:
+            qc_data['ERCC_content'] = quant[ercc.index].sum()
+            quant = quant.drop(ercc.index)
+            quant = quant / quant.sum() * 1e6
+
+        except ValueError:
+            # ERCCs not present
+            pass
 
         qc_data['num_genes'] = (quant > 1.).sum()
         qc_data['MT_content'] = quant[MT].sum()
--- scripts/gather_tech_qc.py
+++ scripts/gather_tech_qc.py
@@ -4,9 +4,9 @@ import readquant
 
 
 @click.command()
-@click.argument('pattern')
-@click.argument('output')
-@click.option('--version')
+@click.argument('pattern', default='salmon/*_salmon_out')
+@click.argument('output', default='sample_qc.csv')
+@click.option('--version', default='0.7.2')
 def main(pattern='salmon/*_salmon_out', output='sample_qc.csv', version=None):
     QCs = readquant.read_qcs(pattern=pattern, tool='salmon', version=version)
     QCs.to_csv(output)
